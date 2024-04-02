3a627690bfe405bc8bad08046ebb27bd414162f0
Enzo Noda <enzo.noda@gmail.com>
2024-04-01 23:47:19 -0300

    add prisma linking to database

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,51 +12,45 @@ import {
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdateUserDTO } from './dto/update-put-user.dto';
 import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
-  async crete(@Body() { email, name, password }: CreateUserDTO) {
-    return { email, name, password };
+  async crete(@Body() data: CreateUserDTO) {
+    return this.userService.create(data);
   }
+
   @Get()
   async read() {
-    return { users: [] };
+    return this.userService.list();
   }
+
   @Get(':id')
   async readOne(@Param('id', ParseIntPipe) id: number) {
-    return { user: {}, id };
+    return this.userService.showOne(id);
   }
+
   @Put(':id')
   async update(
-    @Body() { email, name, password }: UpdateUserDTO,
+    @Body() body: UpdateUserDTO,
     @Param('id', ParseIntPipe) id: number,
   ) {
-    return {
-      method: 'put',
-      email,
-      name,
-      password,
-      id,
-    };
+    return this.userService.update(id, body);
   }
+
   @Patch(':id')
   async updatePartial(
-    @Body() { email, name, password }: UpdatePatchUserDTO,
+    @Body() body: UpdatePatchUserDTO,
     @Param('id', ParseIntPipe) id: number,
   ) {
-    return {
-      method: 'patch',
-      email,
-      name,
-      password,
-      id,
-    };
+    return this.userService.updatePartial(id, body);
   }
+
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
-    return {
-      id,
-    };
+    return this.userService.delete(id);
   }
 }
